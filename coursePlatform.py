0512d9b9ce509e7af4e745d387b6c9c9f78bc371
yonash24 <yonashay87@gmail.com>
2025-03-11 16:45:40 +0200

    finish coursePlatform

--- coursePlatform.py
+++ coursePlatform.py
@@ -1,7 +1,28 @@
+from courses import CourseFullException
+from users import AccessDeniedException
+
+# Handles general input errors across the platform.
+class InvalidInputException(Exception):
+    raise 
+
+
+
+
+#Implements Singleton pattern for a single platform instance.
 class Platform:
-    
-    def __init__(self, courses, users):
-        pass
+    _instance = None
+     
+    @staticmethod
+    def get_instance():
+        if Platform._instance is None:
+            Platform._instance = Platform()
+        return Platform.instance
+        
+    def __init__(self):
+        if Platform._instance is not None:
+            raise RuntimeError("Use get_instance() to access the Platform")
+        self.courses = []
+        self.users = []
 
     def add_courses(self):
         pass
